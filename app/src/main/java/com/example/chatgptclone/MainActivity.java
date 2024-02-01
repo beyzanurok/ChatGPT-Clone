fbd101b942a6b082b2294a4d0db8a125c5f504aa
BEYZANUR OKUDAN <78614478+beyzanurok@users.noreply.github.com>
2024-02-01 16:35:59 +0300

    ChatGPT Clone

--- app/src/main/java/com/example/chatgptclone/MainActivity.java
+++ app/src/main/java/com/example/chatgptclone/MainActivity.java
@@ -103,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Bearer YOUR_API_KEY")
                 .post(body)
                 .build();
 
